44528f660be53452243a70ad37099af633e78f53
amayas mahmoudi <amayasmah562@gmail.com>
2023-11-21 23:48:55 +0100

    🌟 Mise a jour du fichier de creation des tables + tache de creation de tables, transaformation et insertion des donnees

--- dags/dag.py
+++ dags/dag.py
@@ -7,6 +7,8 @@ from airflow.operators.bash import BashOperator
 import pandas as pd 
 import os
 from pathlib import Path
+from sqlalchemy import create_engine
+
 
 # [START default_args]
 default_args = {
@@ -19,33 +21,71 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 # [END default_args]
+
+# Les chemins vers les fichiers sql
 sql_f_createT_path = Path(os.environ['AIRFLOW_HOME'] + '/dags/sql/create_tables.sql')
+# sql_insere_file_path = Path(os.environ['AIRFLOW_HOME'] + '/dags/sql/insert_values.sql')
 
 
 @task(task_id="extract_and_transformation_data")
 def extract_transform_data():
-    # Extract data from various sources
-    print("Data extracted successfully") # Return a serializable result
+
+    #Lecture puis transformation des donnees "urgence" et stockage dans un fichier csv
+
     df_urgence = pd.read_csv(os.path.expandvars("${AIRFLOW_HOME}/data/raw/donnees-urgences-SOS-medecins.csv"),
                         sep=";",
                         dtype="unicode")
     df_urgence = df_urgence.drop(["nbre_acte_tot_f","nbre_acte_tot_h","nbre_acte_corona_f","nbre_acte_corona_h","nbre_acte_tot","nbre_acte_corona"], axis=1)
+    df_urgence["date_de_passage"] = pd.to_datetime(df_urgence["date_de_passage"],errors = "coerce",format='mixed')
+    df_urgence['date_de_passage'] = df_urgence['date_de_passage'].dt.strftime('%Y-%m-%d')
+    df_urgence.columns.values[2] = 'code_age'
+    df_urgence.to_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/donnees-urgences-SOS-medecins.csv"), index=False)
     print(df_urgence)
+
+    #Lecture puis transformation des donnees "tranche d'ages" et stockage dans un fichier csv
 
     df_tranche = pd.read_csv(os.path.expandvars("${AIRFLOW_HOME}/data/raw/code-tranches-dage-donnees-urgences.csv"),
                         sep=";",
                         dtype="unicode")
+    df_tranche.rename(columns={' tranches d\'age': 'tranche', 'Code': 'id'}, inplace=True)
+    df_tranche['tranche'] = df_tranche['tranche'].str.strip()
+    df_tranche.at[0, 'tranche'] = 'tous ages'
+    df_tranche.to_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/code-tranches-dage-donnees-urgences.csv"), index=False)
     print(df_tranche)
-    
+
+    #Lecture puis transformation des donnees "departement" et stockage dans un fichier csv
+
     df_dep = pd.read_json(os.path.expandvars("${AIRFLOW_HOME}/data/raw/departements-region.json"))
+    df_dep.rename(columns={'num_dep': 'id_dep', 'dep_name' : 'nom_dep' , 'region_name':'region'}, inplace=True)
+    df_dep.to_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/departements-region.csv"), index=False)
     print(df_dep)
 
+
+    
 extract = extract_transform_data()
 
 
 @task(task_id="load_data")
 def load_data():
-    print("Loading data into the database")
+
+    # Parametre de connexion
+    utilisateur = "airflow"
+    mot_de_passe = "airflow"
+    host = "172.17.0.1:5435"
+    base_de_donnees = "postgres"
+
+    # Création de la connexion à la base de données
+    engine = create_engine(f'postgresql://{utilisateur}:{mot_de_passe}@{host}/{base_de_donnees}')
+    
+    # Chargement des donnees depuis les fichiers
+    df_tranche = pd.read_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/code-tranches-dage-donnees-urgences.csv"))
+    df_dep = pd.read_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/departements-region.csv"))
+    df_urgence = pd.read_csv(os.path.expandvars("${AIRFLOW_HOME}/data/processed/donnees-urgences-SOS-medecins.csv"))
+    
+    # Insertion des donnees
+    df_tranche.to_sql('tranche_age', con=engine, if_exists='append', index=False,  method='multi', chunksize=1000)
+    df_dep.to_sql('departement', con=engine, if_exists='append', index=False, method='multi', chunksize=1000)
+    df_urgence.to_sql('urgence', con=engine, if_exists='append', index=False, method='multi', chunksize=1000)
 
 insert = load_data()
 
@@ -60,12 +100,7 @@ with DAG(
     tags=['healthcare', 'data-warehousing'],
 ) as dag:
 
-    start = BashOperator(
-        task_id='Start',
-        bash_command='echo Start',
-    
-    )
-
+    # Creation des tables en executant le fichier sql
     create_tables = PostgresOperator(
         task_id='create_tables',
         sql=sql_f_createT_path.read_text(),
@@ -74,4 +109,4 @@ with DAG(
     )
 
 # Define the task dependencies
-start >> create_tables >> extract >> insert
+create_tables >> extract >> [insert]
--- dags/sql/create_tables.sql
+++ dags/sql/create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS departement (
-    id_dep INT PRIMARY KEY,
-    dnom_dep VARCHAR(255),
+    id_dep VARCHAR(5) PRIMARY KEY,
+    nom_dep VARCHAR(255),
     region VARCHAR(255)
 );
 
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS tranche_age (
 
 CREATE TABLE IF NOT EXISTS  urgence (
     id SERIAL PRIMARY KEY,
-    dep VARCHAR(255),
-    code_age INT,
+    dep VARCHAR(5),
     date_de_passage DATE,
+    code_age INT,
     nbre_pass_corona INT,
     nbre_pass_tot INT,
     nbre_hospit_corona INT,
@@ -23,5 +23,7 @@ CREATE TABLE IF NOT EXISTS  urgence (
     nbre_pass_tot_h INT,
     nbre_pass_tot_f INT,
     nbre_hospit_corona_h INT,
-    nbre_hospit_corona_f INT
+    nbre_hospit_corona_f INT,
+    FOREIGN KEY (dep) REFERENCES departement(id_dep),
+    FOREIGN KEY (code_age) REFERENCES tranche_age(id)
 );
