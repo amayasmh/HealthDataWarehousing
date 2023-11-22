bcaeb42a81b148664ebbdde7d77bb9d06be172a9
amayas mahmoudi <amayasmah562@gmail.com>
2023-11-22 14:55:35 +0100

    🌟 Réorganisation du schema de l'entrepot de donnees

--- dags/sql/create_tables.sql
+++ dags/sql/create_tables.sql
@@ -1,5 +1,8 @@
--- Supprimer la table urgence si elle existe
-DROP TABLE IF EXISTS urgence;
+-- Supprimer la table urgence_hospit si elle existe
+DROP TABLE IF EXISTS urgence_hospit;
+
+-- Supprimer la table urgence_mesures si elle existe
+DROP TABLE IF EXISTS urgence_mesures;
 
 -- Supprimer la table tranche_age si elle existe
 DROP TABLE IF EXISTS tranche_age;
@@ -21,20 +24,27 @@ CREATE TABLE IF NOT EXISTS tranche_age (
 
 );
 
-CREATE TABLE IF NOT EXISTS  urgence (
+CREATE TABLE IF NOT EXISTS urgence_mesures (
     id SERIAL PRIMARY KEY,
     dep VARCHAR(5),
     date_de_passage DATE,
     code_age INT,
-    nbre_pass_corona INT,
     nbre_pass_tot INT,
+    nbre_pass_corona INT,
     nbre_hospit_corona INT,
-    nbre_pass_corona_h INT,
-    nbre_pass_corona_f INT,
+    FOREIGN KEY (dep) REFERENCES departement(id)
+    FOREIGN KEY (code_age) REFERENCES tranche_age(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS urgence_hospit (
+    id SERIAL PRIMARY KEY,
+    urgence_mesures_id INT,
     nbre_pass_tot_h INT,
     nbre_pass_tot_f INT,
+    nbre_pass_corona_h INT,
+    nbre_pass_corona_f INT,
     nbre_hospit_corona_h INT,
     nbre_hospit_corona_f INT,
-    FOREIGN KEY (dep) REFERENCES departement(id_dep),
-    FOREIGN KEY (code_age) REFERENCES tranche_age(id)
+    FOREIGN KEY (urgence_mesures_id) REFERENCES urgence_mesures(id)
 );
